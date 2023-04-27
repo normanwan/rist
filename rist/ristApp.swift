f131a9f32f08200cb5db5d0b8c48e0bff1bde30b
normanwan <normanwan97@gmail.com>
2023-04-27 15:47:02 -0400

    Initial Commit

---
+++ rist/ristApp.swift
@@ -0,0 +1,17 @@
+//
+//  ristApp.swift
+//  rist
+//
+//  Created by Norman Wan on 2023-04-27.
+//
+
+import SwiftUI
+
+@main
+struct ristApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
